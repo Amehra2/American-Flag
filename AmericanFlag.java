d4ad462feed61776bd6cc785c797d2f6cc96ac91
Amehra2 <91963304+Amehra2@users.noreply.github.com>
2023-03-29 09:22:40 +0100

    Add files via upload

---
+++ AmericanFlag.java
@@ -0,0 +1,47 @@
+/*
+ *The first step I did was to select "System.out.printIn". By doing this it allowed me to add the stars of the American flag,and more bits that are on the flag.
+ *The second step is that I repeated the same process mentioned in step 1, 9 times. as this is how the flag is layed out.
+ *For the third step, I continued to use "System.out.printIn". However I just changed the lay out, as the flag design is different from the bottom.
+ *For the fourth step, 
+ */
+package american.flag;
+
+
+/**
+ *
+ * @author akashmehra
+ */
+public class AmericanFlag{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("* * * * * * = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
+        System.out.println(" * * * * *  = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
+        System.out.println("* * * * * * = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
+        System.out.println(" * * * * *  = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
+        System.out.println("* * * * * * = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
+        System.out.println(" * * * * *  = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
+        System.out.println("* * * * * * = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
+        System.out.println(" * * * * *  = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
+        System.out.println("* * * * * * = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
+        System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
+        System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
+        System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
+        System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
+        System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
+        System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
+        System.out.println();
+        
+        
+       
+       
+        
+        
+        
+    }
+
+    
+    
+}
